3040f6f196d31c5426c881b7156dd058b74c09ee
Prateek <sharma.prateek182@gmail.com>
2018-05-23 16:08:26 +0530

    Comments Editing now added

--- routes/comments.js
+++ routes/comments.js
@@ -36,6 +36,30 @@ router.post("/campgrounds/:id/comments",isLoggedIn,function(req,res){
     });
 });
 
+//comments edit route
+router.get("/campgrounds/:id/comments/:comment_id/edit",function(req,res){
+    Comment.findById(req.params.comment_id,function(err,foundComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.render("comments/edit",{campground_id:req.params.id,comment: foundComment});
+        }
+    });
+});
+
+
+//update comment route
+router.put("/campgrounds/:id/comments/:comment_id",function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updatedComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+
 //function middleware
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
