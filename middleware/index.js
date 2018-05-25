4036fec9432d64d8c1fe8838e4eca260a4d9decf
Prateek <sharma.prateek182@gmail.com>
2018-05-25 18:37:56 +0530

    Done

--- middleware/index.js
+++ middleware/index.js
@@ -6,17 +6,20 @@ middlewareObj.checkCampgroundOwnership = function(req,res,next){
     if(req.isAuthenticated()){
         Campground.findById(req.params.id,function(err,foundCampground){
             if(err){
+                req.flash("error","Campground not found");
                 res.redirect("back");
             } else {
                 //does user own campground
                 if(foundCampground.author.id.equals(req.user._id)){
                     next();
                 } else {
+                    req.flash("error","Access denied");
                     res.redirect("back");
                 }
             }
         });
     } else {
+        req.flash("error","You need to be logged in");
         res.redirect("back");
     }
 }
